40f7ae94c4859c951e34d2cd2e250debac7006a6
Uri London <uri@london.org.il>
2014-02-07 12:17:27 +0200

    fix last print in ch04

--- globals.py
+++ globals.py
@@ -1,3 +1,6 @@
 
-def pych_url(base):
-    return 'http://www.pythonchallenge.com/pc/def/{base}.html'.format(base=base)
+def pych_url(base, suffix=True):
+    if suffix:
+        return 'http://www.pythonchallenge.com/pc/def/{base}.html'.format(base=base)
+    else:
+        return 'http://www.pythonchallenge.com/pc/def/{base}'.format(base=base)
